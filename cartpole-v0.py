62a886de8de305ac69cb914bca435e58ee799a97
Derek Liang <derek.liang@bcaa.com>
2018-04-25 16:10:27 -0700

    cartpole iteration 1. refine the training data

--- cartpole-v0.py
+++ cartpole-v0.py
@@ -11,7 +11,7 @@ class Meme():
         self.observations = []
         self.targts = []
         model = Sequential()
-        model.add(Dense(512, activation='relu', input_shape=(4,)))
+        model.add(Dense(512, activation='relu', input_shape=(4,),  kernel_initializer='random_uniform', bias_initializer='zeros'))
         model.add(Dense(2, activation='softmax'))
 
         model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.001),
@@ -25,24 +25,40 @@ class Meme():
     def getAction(self, obs):
         return np.argmax(np.array(self.model.predict(np.array([observation])))[0])
 
-    def learn(self, done):
-        if not done:
-            self.observations = self.observations[:-3]
-            self.targts = self.targts[:-3]
-        self.model.fit(np.array(self.observations), np_utils.to_categorical(self.targts, 2), epochs=3, verbose=1)
+    def learn(self):
+        self.model.fit(np.array(self.observations), np_utils.to_categorical(self.targts, 2), shuffle=True, epochs=20, verbose=1)
 
 env = gym.make('CartPole-v0')
 meme = Meme()
-for i_episode in range(20):
+
+for i_episode in range(200):
     observation = env.reset()
-    for t in range(100):
+    observations = []
+    for t in range(1000):
         env.render()
-        action = meme.getAction(observation)
-        print(observation, action)
-        meme.saveTraining(observation, action)
-        #action = env.action_space.sample()
+        action = env.action_space.sample()
+        #print(observation, action)
+        observations.append((observation, action))
+        #meme.saveTraining(observation, action)
+
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
-    meme.learn(done)
+
+    if len(observations) > 6:
+        [ meme.saveTraining(o,a) for (o,a) in observations]
+
+print('learning...')
+meme.learn()
+
+print('show off...')
+observation = env.reset()
+
+while True:
+    env.render()
+    action = meme.getAction(observation)
+    observation, reward, done, info = env.step(action)
+    if done:
+        print("Episode finished after {} timesteps".format(t+1))
+        break
