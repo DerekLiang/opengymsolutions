c66c5a896f5ab5322a793496d74ec8f656585459
Derek Liang <derek.liang.ca@gmail.com>
2018-04-25 23:26:43 -0700

    cartpole iteration 2

--- cartpole-v0.py
+++ cartpole-v0.py
@@ -5,60 +5,78 @@ from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.optimizers import SGD
 from keras.utils import np_utils
+import bcolz
 
 class Meme():
     def __init__(self):
-        self.observations = []
-        self.targts = []
         model = Sequential()
+        model.add(Dense(512, activation='relu', input_shape=(4,),  kernel_initializer='random_uniform', bias_initializer='zeros'))
         model.add(Dense(512, activation='relu', input_shape=(4,),  kernel_initializer='random_uniform', bias_initializer='zeros'))
         model.add(Dense(2, activation='softmax'))
 
-        model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.001),
+        model.compile(loss='categorical_crossentropy', optimizer='adadelta',
                     metrics=['accuracy'])
+        model.summary()
         self.model = model
-
-    def saveTraining(self, observation, action):
-        self.observations.append(observation)
-        self.targts.append(action)
 
     def getAction(self, obs):
         return np.argmax(np.array(self.model.predict(np.array([observation])))[0])
 
     def learn(self):
-        self.model.fit(np.array(self.observations), np_utils.to_categorical(self.targts, 2), shuffle=True, epochs=20, verbose=1)
+        states = bcolz.carray(rootdir='data/cartpole_state', mode='r')
+        targets = bcolz.carray(rootdir='data/cartpole_target', mode='r')
+        states = states.reshape((-1,4))
+        targets = np_utils.to_categorical(targets, 2)
+
+        self.model.fit(states, targets, shuffle=True, epochs=20, verbose=1)
 
 env = gym.make('CartPole-v0')
 meme = Meme()
 
-for i_episode in range(2000):
-    observation = env.reset()
-    observations = []
-    for t in range(1000):
-        env.render()
-        action = env.action_space.sample()
-        #print(observation, action)
-        observations.append((observation, action))
-        #meme.saveTraining(observation, action)
+def generate_data():
+    states = bcolz.carray([], rootdir='data/cartpole_state', mode='w')
+    targets = bcolz.carray([], rootdir='data/cartpole_target', mode='w', dtype='i')
+    sample = 1
+    while sample < 2000:
+        observation = env.reset()
+        observations = []
+        for t in range(1000):
+            env.render()
+            action = env.action_space.sample()
+            #print(observation, action)
+            observations.append((observation, action))
+            #meme.saveTraining(observation, action)
 
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+            observation, reward, done, info = env.step(action)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
 
-    if len(observations) > 6:
-        [ meme.saveTraining(o[3:-3],a) for (o,a) in observations]
+        if len(observations) > 20:
+            observations = observations[5:-5]
+            for (o,a) in observations:
+                states.append(o)
+                targets.append(a)
+            states.flush()
+            targets.flush()
+            sample += 1
+
+
+#print('generating learning data')
+#generate_data()
 
 print('learning...')
 meme.learn()
 
 print('show off...')
 
-observation = env.reset()
 while True:
-    env.render()
-    action = meme.getAction(observation)
-    observation, reward, done, info = env.step(action)
-    if done:
-        print("Episode finished after {} timesteps".format(t+1))
-        break
+    observation = env.reset()
+    for t in range(1000**2):
+        env.render()
+        action = meme.getAction(observation)
+        observation, reward, done, info = env.step(action)
+        print('action:', action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
