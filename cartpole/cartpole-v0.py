94fdbc3e3c27316cd238ef41e4cd1b27515b5777
Derek Liang <derek.liang@bcaa.com>
2018-04-27 16:09:30 -0700

    wip

--- cartpole/cartpole-v0.py
+++ cartpole/cartpole-v0.py
@@ -8,15 +8,26 @@ from keras.utils import np_utils
 import bcolz
 import itertools
 
+learnDepth = 3
+
 class Meme():
-    def getAction(self, ob):
+    def getAction(self, obs, currentOb):
         resultMove = []
         resultScore = []
+
         for moves  in itertools.product([0,1], repeat=4):
-            o = ob
+            o = obs[:-learnDepth]
+            co = currentOb
             for m in moves:
-                s = np.insert(o, len(o), m-0.5)
-                o = self.model.predict(np.array([s]))[0]
+                o.append((co, m))
+                states = []
+                for j in range(learnDepth):
+                    (po,pa) = observations[i-1-j]
+                    state = np.insert(po, len(po), pa-0.5)
+                    states.append(state)
+
+                co = self.model.predict(np.array([states]))[0]
+
             resultMove.append(moves[0])
             resultScore.append(self.score(o))
 
@@ -33,14 +44,13 @@ class Meme():
         #model.add(Dense(512, activation='relu', input_shape=(4,),  kernel_initializer='random_uniform', bias_initializer='zeros'))
         model.add(Dense(4))
 
-        model.compile(loss='mse', optimizer='adadelta',
-                    metrics=['accuracy'])
+        model.compile(loss='mse', optimizer='adadelta', metrics=['accuracy'])
         model.summary()
         self.model = model
 
         states = bcolz.carray(rootdir='data/cartpole_state', mode='r')
         targets = bcolz.carray(rootdir='data/cartpole_target', mode='r')
-        states = states.reshape((-1,5))
+        states = states.reshape((-1,5*learnDepth))
         targets = targets.reshape((-1,4))
 
         self.model.fit(states, targets, shuffle=True, epochs=180, verbose=1)
@@ -63,20 +73,19 @@ def generate_data():
         for t in range(1000):
             env.render()
             action = env.action_space.sample()
-            #print(observation, action)
             observations.append((observation, action))
-            #meme.saveTraining(observation, action)
-
             observation, reward, done, info = env.step(action)
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
                 break
 
-        for i in range(1, len(observations)):
-            (po,pa) = observations[i-1]
+        for i in range(learnDepth, len(observations)):
+            for j in range(learnDepth):
+                (po,pa) = observations[i-1-j]
+                state = np.insert(po, len(po), pa-0.5)
+                states.append(state)
+
             (co,_) = observations[i]
-            state = np.insert(po, len(po), pa-0.5)
-            states.append(state)
             targets.append(co)
 
         states.flush()
@@ -96,11 +105,21 @@ meme.loadModel()
 print('show off...')
 while True:
     observation = env.reset()
+    obs = []
     for t in range(1000**2):
         env.render()
-        (action, predict) = meme.getAction(observation)
+
+        if len(obs) >= learnDepth:
+            (action, predict) = meme.getAction(observation[:-learnDepth])
+        else:
+            action = env.action_space.sample()
+
+        obs.append((observation, action))
         observation, reward, done, info = env.step(action)
-        print(action, observation, (observation - predict)*100/observation)
+
+        if len(obs) > learnDepth:
+            print(action, observation, (observation - predict)*100/observation)
+
         if abs(observation[2])> 0.17:
             print('dangerous')
         if done:
